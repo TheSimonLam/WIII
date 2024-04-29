8afcf2b8f1fa4396cad61c207c29ad6bf275f9ee
Simon Lam <thesimonlam@gmail.com>
2024-04-29 23:43:36 +0100

    adding api

---
+++ api.ts
@@ -0,0 +1,9 @@
+export const getAutocompelteData = async () => {
+  const res = await fetch("http:www.api.com", { body: JSON.stringify({}) });
+  return res;
+};
+
+export const getAssetData = async () => {
+  const res = await fetch("http:www.api.com", { body: JSON.stringify({}) });
+  return res;
+};
